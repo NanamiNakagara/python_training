1802c784cbcdb8835085b9734b92af80c330f167
NanamiNakagara <nanami_nakagara@niandc.co.jp>
2024-11-05 17:20:17 +0530

    Day2 lower

---
+++ Day_2.3_basic_string_methods_task.py
@@ -0,0 +1,22 @@
+# Task 1.1
+secret_message = "   Programming in Python is not only powerful but also fun!   "
+
+# Clue:
+# Slice operator
+
+# Expected Output
+# "PYTHON-POWERFUL"
+
+
+#a=secret_message.upper()
+#print(a[18:24]+'-'+a[37:45])
+
+# Task 1.2
+flipped_message = "!nuf sseldnE dna seitinutroppo lufrewop htiw nohtyP"
+
+# Expected Output
+# "python 🗡️ powerful 🌸"
+a=flipped_message.lower()
+a=a[::-1]
+print(a[0:7]+' 🗡️ '+a[12:20]+' 🌸 ')
+print('🤷‍♀️🐵🫁🫀🧑‍🍼💘❤️‍🔥')
