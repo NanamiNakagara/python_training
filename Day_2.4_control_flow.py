db29f01504412714ac12023a22cb5b42eea9ba24
NanamiNakagara <nanami_nakagara@niandc.co.jp>
2024-11-13 16:37:48 +0530

    my learnings

---
+++ Day_2.4_control_flow.py
@@ -0,0 +1,23 @@
+# Task
+# Get two person name
+
+# Case 1:
+# Luffy, Zoro
+# 173cm, 163cm
+# Expected
+# Luffy is taller than Zoro by 10cm
+Captian_name=input('tell me ONE PIECE captain name')
+Captian_height=input('tell me ONE PIECE captain height')
+subcaptain captain_name=input("tell me ONE PIECE sub captain name")
+subcaptain captain_height=input('tell me ONE PIECE sub captain height')
+if Captian_height >= subcaptain_height:
+    print('Captian is taller than sub captain by 10cm')
+
+subcaptain=
+if Captian >= sub captain:
+    print('Captian is taller than sub captain by 10cm')
+# Case 2:
+# Luffy, sub captain
+# 173cm, 185cm
+# Expected
+# sub captain is taller than Luffy by 12cm
